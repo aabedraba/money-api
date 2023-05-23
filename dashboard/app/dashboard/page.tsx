7069dc02b37b92ed6d2db8a692ea25b3dc87f6a0
aabedraba <aabedraba@gmail.com>
2023-05-23 17:59:14 +0200

    feat: improve dashboard

--- dashboard/app/dashboard/page.tsx
+++ dashboard/app/dashboard/page.tsx
@@ -5,22 +5,30 @@ export default async function DashboardPage() {
     // center the content
     <div className="flex items-center justify-center">
       <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-        <div className="text-center items-center">
+        <div className="items-center">
           <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl pb-8">
             To use the API follow the instructions below.
           </h1>
-          <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl">
-            Go to ${zuploUrl}/docs and sign in
+          <p className="max-w-[700px] text-lg  sm:text-xl">
+            Go to{" "}
+            <a className="text-blue-500" href={zuploUrl + "/docs"}>
+              {zuploUrl}/docs
+            </a>{" "}
+            and sign in
           </p>
           <img src="/docs-signin.png" alt="docs" />
-          <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl">
-            Once logged in, click on <code>Create Key</code>
+          <p className="max-w-[700px] text-lg sm:text-xl">
+            Once logged in, click on <code>Create Key</code>:
           </p>
           <img src="/bucket-create-key.png" alt="create key" />
-          <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl">
-            Make an authenticated API request
+          <p className="max-w-[700px] text-lg  sm:text-xl">
+            Make an authenticated API request:
           </p>
-          <code>curl</code>
+          <code>
+            curl --request GET \ --url<br />
+            https://teal-hornet-main-79f7919.zuplo.app/v1/todos \ --header <br />
+            &apos;Authorization: Bearer YOUR_KEY_HERE&apos;
+          </code>
         </div>
       </section>
     </div>
