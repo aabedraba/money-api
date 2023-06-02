1290ae25720c81f9ab319956638b937b8d29c2df
Abdallah <aabedraba@gmail.com>
2023-06-02 13:28:18 +0200

    remove need for a db

--- dashboard/app/api/auth/[...nextauth]/route.ts
+++ dashboard/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,7 @@
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import NextAuth, { NextAuthOptions } from "next-auth"
 import Auth0Provider from "next-auth/providers/auth0"
 
-import prisma from "@/lib/prisma"
-
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
   providers: [
     Auth0Provider({
       clientId: process.env.AUTH0_CLIENT_ID || "",
